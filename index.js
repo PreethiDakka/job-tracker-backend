2fd346bf7277befa138dc53fc0b78badb24f298e
Sahithi Dakka <preethidakka@gmail.com>
2026-02-23 11:38:46 +0530

    Changes added to index.js

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
-require("dotenv").config();
 
-const jobRoutes = require("./routes/jobRoutes");
 const authRoutes = require("./routes/authRoutes");
+const jobRoutes = require("./routes/jobRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/jobs", jobRoutes);
 app.use("/auth", authRoutes);
+app.use("/jobs", jobRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
+
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("MongoDB Connection Error:");
-    console.log(err.message);
+    console.log("MongoDB Connection Error:", err.message);
   });
